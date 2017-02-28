2712927782ce0e8e5e0c82dd058a21745b3d6277
Joshua Bradford <jjyet@yahoo.com>
2017-02-28 13:13:16 -0500

    mostly final for of this configuration. all tests passing all old properties deleted.

--- src/js/main.js
+++ src/js/main.js
@@ -2,34 +2,6 @@ function GildedRose (sellIn, quality, name) {
   this.name = name;
   this.sellIn = sellIn;
   this.quality = quality;
-  if(this.name === 'normal'){
-    this.degradationRate = -1;
-    this.minQuality = 0;
-    this.sellbyRate = -1;
-  }
-  if(this.name === 'Aged Brie'){
-    this.degradationRate = 1;
-    this.maxQuality = 50;
-    this.sellbyRate = -1;
-  }
-  if(this.name === 'Sulfuras, Hand of Ragnaros'){
-    this.degradationRate = 0;
-    this.minQuality = 80;
-    this.maxQuality = 80;
-    this.sellbyRate = 0;
-  }
-  if(this.name === 'Backstage passes to a TAFKAL80ETC concert'){
-    this.degradationRate = 1;
-    this.degradationRateMedium = 2;
-    this.degradationRateClose = 3;
-    this.maxQuality = 50;
-    this.sellbyRate = -1;
-  }
-  if(this.name === 'Conjured Mana Cake'){
-    this.degradationRate = -2;
-    this.minQuality = 0;
-    this.sellbyRate = -1;
-  }
 }
 
 GildedRose.prototype.degrade = function (name){
@@ -63,11 +35,16 @@ GildedRose.prototype.sellDegrade = function (){
   if(this.name !== 'Sulfuras, Hand of Ragnaros'){this.sellIn--;}
 };
 
+GildedRose.prototype.qualityCheck = function (){
+  if(this.name === 'normal'){if(this.quality < 0){this.quality = 0}}
+  if(this.name === 'Aged Brie'){if(this.quality > 50){this.quality = 50}}
+  if(this.name === 'Backstage passes to a TAFKAL80ETC concert'){if(this.quality > 50){this.quality = 50}}
+  if(this.name === 'Conjured Mana Cake'){if(this.quality < 0){this.quality = 0}}
+};
+
 GildedRose.prototype.tick = function () {
   this.degrade(this.name);
-  if(this.quality > this.maxQuality){this.quality = this.maxQuality;}
-  if(this.quality < this.minQuality){this.quality = this.minQuality;}
-  //this.sellIn += this.sellbyRate;
+  this.qualityCheck();
   this.sellDegrade();
 };
 
